387f8a6cb7653489904ab0d29643dacd1eb119ca
kkyouma <144501527+kkyouma@users.noreply.github.com>
2024-02-25 04:17:32 -0300

    add age and education analysis

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -2,28 +2,26 @@ import pandas as pd
 
 
 def calculate_demographic_data(print_data=True):
-    # Read data from file
-    df = None
+    df = pd.read_csv("adult.data.csv")
+    total_people = df.shape[0]
 
-    # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = None
+    race_count = df["race"].value_counts()
 
     # What is the average age of men?
-    average_age_men = None
+    average_age_men = df[df["sex"] == "Male"]["age"].mean()
 
-    # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+    bachelors_degrees = df[df["education"] == "Bachelors"].shape[0]
+    percentage_bachelors = (bachelors_degrees / total_people) * 100
 
-    # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
-    # What percentage of people without advanced education make more than 50K?
+    higher_education = df[df["education"].isin(["Bachelors", "Master", "Doctorate"])]
+    lower_education = df[~df["education"].isin(["Bachelors", "Master", "Doctorate"])]
 
-    # with and without `Bachelors`, `Masters`, or `Doctorate`
-    higher_education = None
-    lower_education = None
-
-    # percentage with salary >50K
-    higher_education_rich = None
-    lower_education_rich = None
+    higher_education_rich = (
+        higher_education[higher_education["salary"] == ">50K"] / total_people
+    ) * 100
+    lower_education_rich = (
+        lower_education[lower_education["salary"] == ">50K"] / total_people
+    ) * 100
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
     min_work_hours = None
